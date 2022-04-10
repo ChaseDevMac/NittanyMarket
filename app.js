31720682f2c13f7dc7169921bc153178feb26549
Chase <ChaseDevMac@protonmail.com>
2022-04-09 20:51:32 -0500

    add home page with navbar

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const ejsMate = require('ejs-mate');
 
 PORT = 8080;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', ejsMate);
+
+//Middleware
 
 const db = require('./utils/database');
 
 app.get('/', (req, res) => {
-  res.send('MariaDB connected');
+  res.render('home');
 });
 
 app.listen(PORT, () =>{
