83da0384f6224d9ea6a8fd65bef0862087b1f783
Chase <ChaseDevMac@protonmail.com>
2022-04-09 21:28:25 -0500

    feat: implement login validation

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const ejsMate = require('ejs-mate');
+const bcrypt = require('bcrypt');
+
+const User = require('./models/users');
 
 PORT = 8080;
 
@@ -10,11 +13,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', ejsMate);
 
 //Middleware
+app.use(express.urlencoded({extended: true}));
 
 const db = require('./utils/database');
 
+const validateLogin = async function (req, res, next) {
+  const { email, password } = req.body.user;
+  const foundUser = await User.findByPk(email);
+  if (!foundUser)
+    return res.send('Wrong password or email');
+  const comparePassword = bcrypt.compareSync(password, foundUser.password);
+  if (!comparePassword)
+    return res.send('Wrong password or email');
+  next();
+}
+
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('users/login');
+});
+
+app.post('/login', validateLogin, (req, res) => {
+  res.redirect('/');
 });
 
 app.listen(PORT, () =>{
