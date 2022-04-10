e12daddd7006e6b2bf1ca235940460c906c225f5
Chase <ChaseDevMac@protonmail.com>
2022-04-10 09:35:06 -0500

    feat: add sessions and logout functionality

--- app.js
+++ app.js
@@ -3,17 +3,35 @@ const path = require('path');
 const app = express();
 const ejsMate = require('ejs-mate');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 const User = require('./models/users');
 
-PORT = 8080;
+const PORT = 8080;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', ejsMate);
 
+const sessionConfig = {
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // sameSite: true,
+    // maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
 //Middleware
 app.use(express.urlencoded({extended: true}));
+app.use(session(sessionConfig));
+app.use((req, res, next) => {
+  console.log(req.session);
+  res.locals.user = req.session.email;
+  next();
+})
 
 const db = require('./utils/database');
 
@@ -28,6 +46,14 @@ const validateLogin = async function (req, res, next) {
   next();
 }
 
+const isLoggedIn = async function (req, res, next) {
+  console.log('in isLoggedIn');
+  if (!req.session.email) {
+    return res.redirect('/login');
+  }
+  next();
+}
+
 app.get('/', (req, res) => {
   res.render('home');
 });
@@ -37,6 +63,8 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', validateLogin, (req, res) => {
+  console.log(req.body);
+  req.session.email = req.body.user.email;
   res.redirect('/');
 });
 
@@ -50,11 +78,18 @@ app.post('/register', async (req, res) => {
     const hashPass = bcrypt.hashSync(password, 12);
     const newUser = User.build({ email, password: hashPass });
     await newUser.save();
-    res.redirect('/login');
+    req.session.email = newUser.email;
+    return res.redirect('/');
   } catch (err) {
     console.log(err);
+    res.send('Error');
   }
 });
+
+app.get('/logout', isLoggedIn, (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+})
 
 app.listen(PORT, () =>{
   console.log(`Listening on ${PORT}`);
