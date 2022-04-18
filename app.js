889805154a94ed305ca56e5bc7ef97a34310ef68
Chase <ChaseDevMac@protonmail.com>
2022-04-18 10:00:45 -0500

    feat: implement redis to manage sessions

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const path = require('path');
 const app = express();
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
+const redis = require('redis');
+const redisClient = redis.createClient({ legacyMode: true });
+const redisStore = require('connect-redis')(session);
+redisClient.connect();
 
 const PORT = 8080;
 
@@ -10,15 +14,26 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', ejsMate);
 
+redisClient.on('error', (err) => {
+  console.log('Redis Error: ', err);
+});
+
 const sessionConfig = {
-  secret: 'secret',
+  secret: 'PennStateIsBetterThanNittanyState',
   resave: false,
   saveUninitialized: true,
+  store: new redisStore({
+    host: 'localhost',
+    port: 6379,
+    client: redisClient,
+    ttl: 86400,
+  }),
   cookie: {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
+app.use(session(sessionConfig));
 
 const {testModels} = require('./utils/test_models');
 app.get('/test', async (req, res) => {
@@ -29,7 +44,6 @@ app.get('/test', async (req, res) => {
 
 //Middleware
 app.use(express.urlencoded({extended: true}));
-app.use(session(sessionConfig));
 app.use((req, res, next) => {
   res.locals.user = req.session.email;
   next();
