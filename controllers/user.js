5ed36b17171d5247969c227f191281b8b342f51a
Chase <ChaseDevMac@protonmail.com>
2022-04-24 20:56:09 -0500

    chore: clean up and write comments for user controller

--- controllers/user.js
+++ controllers/user.js
@@ -2,27 +2,31 @@ const { ProductListing, Rating } = require('../models');
 const { sequelize } = require('../utils/database');
 const { Op } = require('sequelize');
 
-
+// display the user's public profile
 module.exports.renderUser = async (req, res) => {
   const userEmail = req.params.email;
+
+  // find all the listings under the seller
   const listings = await ProductListing.findAll({
     where: {
-      [Op.and]: {
-        sellerEmail: userEmail,
-        quantity: {[Op.gt]: 0 },
-        removeDate: {[Op.is]: null},
-      }
-    },
+      sellerEmail: userEmail,
+      quantity: {[Op.gt]: 0 },
+      removeDate: {[Op.is]: null},
+    }
   });
+
+  // find all the ratings associated with the seller
   const ratings = await Rating.findAll({where: {sellerEmail: userEmail}})
-  const sellerRating = await Rating.findAll({
+
+  // calculate the overall average rating for the seller
+  const sellerRating = await Rating.findOne({
     where: {sellerEmail: userEmail}, 
     attributes: [[sequelize.fn('avg', sequelize.col('rating')), 'avgRating']]
   });
-  res.locals.user = userEmail.split('@')[0];
+  res.locals.user = userEmail.split('@')[0]; // remove the email provider of address
   res.locals.listings = listings;
   res.locals.ratings = ratings;
-  res.locals.sellerRating = sellerRating[0].dataValues.avgRating;
+  res.locals.sellerRating = sellerRating.dataValues.avgRating;
   res.render('users/show');
 };
 
