a0c22ba703644f7bb53e0b4d7ea6fb4a1d9ef9d1
Chase <ChaseDevMac@protonmail.com>
2022-04-24 20:25:38 -0500

    chore: write comments for listing controller

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,7 +1,9 @@
 const { sequelize } = require('../utils/database');
 const { Category, ProductListing, Review, Rating } = require('../models/');
 
+// display the product listing creation form
 module.exports.renderListingForm = async (req, res) => {
+  // find all the categories for dropdown menu
   const categories = await Category.findAll();
   const allCategories = [];
   for (let category of categories) {
@@ -10,14 +12,16 @@ module.exports.renderListingForm = async (req, res) => {
       allCategories.push(categoryName);
     }
   }
+  // sort the categories alphanumerically
   res.locals.categories = allCategories.sort();
   res.render('listings/create');
 }
 
+// insert a new listing into database
 module.exports.createListing = async (req, res) => {
   const sellerEmail = req.session.email;
   const listing = req.body.listing;
-  const listingId = Math.floor(Math.random() * 11111111);
+  const listingId = Math.floor(Math.random() * 11111111); // generate a random new listingId
   const newListing = await ProductListing.create({
     sellerEmail,
     listingId,
@@ -32,10 +36,15 @@ module.exports.createListing = async (req, res) => {
   res.redirect(`/listings/${newListing.listingId}`);
 }
 
+// display a product listing
 module.exports.showListing = async (req, res) => {
   const { listingId } = req.params;
   const listing = await ProductListing.findOne({where: {listingId: listingId}});
+
+  // find all the reviews associated with this product listing
   const reviews = await Review.findAll({where: {listingId: listingId}});
+
+  // find the average rating for the given seller
   const sellerRating = await Rating.findAll({
     where: {sellerEmail: listing.sellerEmail}, 
     attributes: [[sequelize.fn('avg', sequelize.col('rating')), 'avgRating']]
@@ -47,6 +56,7 @@ module.exports.showListing = async (req, res) => {
   res.render('listings/show');
 }
 
+// delete a product listing
 module.exports.deleteListing = async (req, res) => {
   const { listingId } = req.params;
   const listing = await ProductListing.findOne({where: {listingId: listingId}});
@@ -54,11 +64,13 @@ module.exports.deleteListing = async (req, res) => {
   res.redirect('/mynm/listings');
 }
 
+// render the form for editing a product listing
 module.exports.renderEditForm = async (req, res) => {
   res.locals.listing = await ProductListing.findOne({where: {listingId: req.params.listingId}});
   res.render('listings/edit');
 }
 
+// edit a listing with the new information
 module.exports.editListing = async (req, res) => {
   const { listingId } = req.params;
   const editListing = req.body.listing;
